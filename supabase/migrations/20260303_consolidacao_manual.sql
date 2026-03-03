49dbc18d9258e4abc0c97c491878d78a91f31813
Leandro Lastori <leandrolastori@gmail.com>
2026-03-03 20:05:38 -0300

    chore: consolidar estado real do banco apos ajustes manuais

---
+++ supabase/migrations/20260303_consolidacao_manual.sql
@@ -0,0 +1,14 @@
+﻿drop trigger if exists "trg_validate_offer_mapping_funnel_project" on "public"."offer_mappings";
+
+alter table "public"."meta_insights" drop constraint "meta_insights_project_adacct_campaign_adset_ad_date_key";
+
+drop function if exists "public"."delete_funnel_safe"(p_funnel_id uuid);
+
+drop view if exists "public"."v_offer_mappings_funnel_project_inconsistencies";
+
+drop function if exists "public"."validate_offer_mapping_funnel_project"();
+
+drop index if exists "public"."meta_insights_project_adacct_campaign_adset_ad_date_key";
+
+
+

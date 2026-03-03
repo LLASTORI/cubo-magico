39cf0d872e91a78258622a7a540237d603ee87d8
Leandro Lastori <leandrolastori@gmail.com>
2026-03-03 20:15:39 -0300

    fix: remove BOM from migration file

--- supabase/migrations/20260303_consolidacao_manual.sql
+++ supabase/migrations/20260303_consolidacao_manual.sql
@@ -1,4 +1,4 @@
-﻿drop trigger if exists "trg_validate_offer_mapping_funnel_project" on "public"."offer_mappings";
+drop trigger if exists "trg_validate_offer_mapping_funnel_project" on "public"."offer_mappings";
 
 alter table "public"."meta_insights" drop constraint "meta_insights_project_adacct_campaign_adset_ad_date_key";
 
